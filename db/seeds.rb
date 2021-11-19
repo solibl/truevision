ae5a9868e368dd1f8eb7b724fba4721540824b34
Brian Lee <brianleedongjun@gmail.com>
2021-11-19 09:43:19 -0800

    Create initial locations and Admin User.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create(name: '810')
+Location.create(name: 'SL')
+Location.create(name: '49th')
+
+fire_factory = Location.where(name: '810')
+sl = Location.where(name: 'SL')
+forty_ninth = Location.where(name: '49th')
+
+User.create(location_id: fire_factory, first_name: 'Tim', last_name: 'Spelone', authorization_level: 'Admin', email: '810veg@true-vision.co', password: 'admin')
